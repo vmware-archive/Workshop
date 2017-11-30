682ac3c06426869f48c484730a4e6edb849ef77b
Dave Tillman <dtillman@pivotal.io>
2017-11-30 11:59:02 -0700

    Update Hystrix refs, add createclient script

--- Final/scripts/createCloudFoundryServices.cmd
+++ Final/scripts/createCloudFoundryServices.cmd
@@ -5,4 +5,4 @@ cf create-service p-service-registry standard myDiscoveryService
 cf create-service p-mysql 100mb myMySqlService
 cf create-service p-redis shared-vm myRedisService
 cf create-service p-circuit-breaker-dashboard standard myHystrixService
-cf cups myOAuthService -p "{\"client_id\": \"myTestApp\",\"client_secret\": \"myTestApp\",\"uri\": \"uaa://login.system.testcloud.com\"}"
+cf cups myOAuthService -p "{\"client_id\": \"dave1App\",\"client_secret\": \"dave1Secret\",\"uri\": \"uaa://login.sys.selma.cf-app.com\"}"
